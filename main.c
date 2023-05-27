aa0f9057c871dfacde00e258d02e77c6d9da6642
DCM <DCM@BUKAOPU.com>
2023-05-28 00:13:07 +0800

    第一次提交

---
+++ main.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include"LinkdeList.h"
+int main()
+{
+    biNode* first=creat_linkedlist_v2();
+    //delete_liank_all(first);
+    // if(first->data==NULL)
+    // {
+    //     printf("已删除链表！\n");
+    // }
+    fushu(first);
+}
